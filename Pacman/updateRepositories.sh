7726b65142fe6f1272aa809d1f2896f93337e220
Bruno Fontes <brunofontes.github@mailnull.com>
2018-07-23 12:26:29 -0300

    Changing aur to aurman and including Service to mount Multimedia partition

--- Pacman/updateRepositories.sh
+++ Pacman/updateRepositories.sh
@@ -1,26 +1,13 @@
 echo -n Please type your sudo password:
 read -s sudoPass
 echo;
-check_pkgversion() {
-	newver=$(curl -s "https://aur.archlinux.org/rpc.php?type=info&arg=$1" | awk -F : '{print $10}' | awk -F , '{print $1}' | sed 's/"//g')
-	currentver=$(pacman -Qi $1 | awk '{print $3}' | head -n 2 | tail -n 1)
-	if [[ $newver != $currentver ]]; then
-		echo 1
-	fi
-}
-
+echo "$sudoPass" | sudo -S pacman-mirrors -g
 echo
 echo Updating with Pacman...
 echo
-echo "$sudoPass" | sudo -S pacman -Syu --color always && sudo pacman-mirrors -g
-echo
-aurpkgs=$(pacman -Qm | awk '{print $1}')
-for line in $aurpkgs
-do
-	vcheck=$(check_pkgversion $line)
-	if [[ $vcheck == 1 ]]; then
-		source ~/aur.sh "$line" "$sudoPass"
-	fi
-done
+echo "$sudoPass" | sudo -S pacman -Syu --color always
+echo;
+aurman -Su --color always
+echo;
 echo "$sudoPass" | yes | (sudo -S pacman -Rns $(pacman -Qtdq) --color always)
 notify-send "Update script has finished!"
