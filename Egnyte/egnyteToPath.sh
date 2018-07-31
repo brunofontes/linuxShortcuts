4737ae5bc7e0b6a87683915e18cd1fe381ebd607
Bruno Fontes <developer@brunofontes.net>
2018-07-31 11:23:12 -0300

    Adding egnyteToPath script

--- Egnyte/egnyteToPath.sh
+++ Egnyte/egnyteToPath.sh
@@ -1,3 +1,3 @@
 cpboard=$(xsel -b)
-cpboard=$(echo "$cpboard" | sed -e 's/\//\\/g' -e 's/https:\/\/oxo.egnyte.com\/app\/index.do#storage\/files\/1\//Z:\//')
-echo "$cpboard" | xsel
+path=$(echo "$cpboard" | sed -e 's/\//\\/g' -e 's/%20/ /g' -e 's/https:\\\\oxo\.egnyte\.com\\app\\index\.do\#storage\\files\\1\\/Z:\\/g')
+echo "$path" | xsel
