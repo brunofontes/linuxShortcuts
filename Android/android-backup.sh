85a8a60b7472a2519873e108af3d340653ea58e6
Bruno Fontes <developer@brunofontes.net>
2019-04-12 18:57:49 -0300

    Android - added: Alert after finishing backup/restore

--- Android/android-backup.sh
+++ Android/android-backup.sh
@@ -1,1 +1,2 @@
 adb backup -apk -shared -all -f /home/bruno/Documents/Dropbox/Backups/android_backup.ab
+kdialog --title "Android Backup Complete" --msgbox "Android Backup finished successfully"
---
+++ Android/android_recover.sh
@@ -0,0 +1,2 @@
+adb restore /home/bruno/Documents/Dropbox/Backups/android_backup.ab
+kdialog --title "Android Restore Complete" --msgbox "Android Restore finished successfully"
