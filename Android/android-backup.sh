f06d59da1f327971e5d5afb88f907fa11a184e96
Bruno Fontes <developer@brunofontes.net>
2019-04-12 18:59:49 -0300

    Fixing android alert
    Alert message was being used even when the backup/restore was not
    completed succesfully. Fixed it using the && operator.

--- Android/android-backup.sh
+++ Android/android-backup.sh
@@ -1,2 +1,1 @@
-adb backup -apk -shared -all -f /home/bruno/Documents/Dropbox/Backups/android_backup.ab
-kdialog --title "Android Backup Complete" --msgbox "Android Backup finished successfully"
+adb backup -apk -shared -all -f /home/bruno/Documents/Dropbox/Backups/android_backup.ab && kdialog --title "Android Backup Complete" --msgbox "Android Backup finished successfully"
--- Android/android_recover.sh
+++ Android/android_recover.sh
@@ -1,2 +1,1 @@
-adb restore /home/bruno/Documents/Dropbox/Backups/android_backup.ab
-kdialog --title "Android Restore Complete" --msgbox "Android Restore finished successfully"
+adb restore /home/bruno/Documents/Dropbox/Backups/android_backup.ab && kdialog --title "Android Restore Complete" --msgbox "Android Restore finished successfully"
