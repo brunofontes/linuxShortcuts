ab9a0921934bb33214f54c1a0bd1392831745dd1
Bruno Fontes <developer@brunofontes.net>
2019-04-13 20:59:28 -0300

    Fixing the file path
    The first script was saving the backup file under the Dropbox folder.
    That's not a good idea, as my Dropbox has a limited space and phone
    backups has about 8GB of data.

--- Android/android_recover.sh
+++ Android/android_recover.sh
@@ -1,1 +1,1 @@
-adb restore /home/bruno/Documents/Dropbox/Backups/android_backup.ab && kdialog --title "Android Restore Complete" --msgbox "Android Restore finished successfully"
+adb restore /run/media/bruno/Multimedia/My\ Backups/android_backup.ab && kdialog --title "Android Restore Complete" --msgbox "Android Restore finished successfully"
