4a915b7a39777b42c78c67892d5a2d402063915a
Bruno Fontes <developer@brunofontes.net>
2019-06-10 10:19:55 -0300

    Improving Android ADB Backup report
    Included info about the exported file size and time of execution at the
    end of the run command.

--- Android/android-backup.sh
+++ Android/android-backup.sh
@@ -1,1 +1,4 @@
-adb backup -apk -shared -all -f /run/media/bruno/Multimedia/My\ Backups/android_backup.ab && kdialog --title "Android Backup Complete" --msgbox "Android Backup finished successfully"
+
+time adb backup -apk -shared -all -f /run/media/bruno/Multimedia/My\ Backups/android_backup.ab && kdialog --title "Android Backup Complete" --msgbox "Android Backup finished successfully"
+echo
+ls -lh /run/media/bruno/Multimedia/My\ Backups/android_backup.ab | gawk '{ printf $5 " "; for(i=6;i<=NF;i++) printf $i " "; print "\n" }'
