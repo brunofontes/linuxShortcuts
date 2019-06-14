e588f9685b79ca5110c09be49be6d6fcc2608564
Bruno Fontes <developer@brunofontes.net>
2019-06-14 16:52:52 -0300

    Updating with the changes on the external HD

--- Backup/.Backup_HD_1TB.sh
+++ Backup/.Backup_HD_1TB.sh
@@ -1,10 +1,7 @@
 YEARMONTH=`date +%Y/%m-%b`
 YEAR=`date +%Y`
-HDPath="/run/media/bruno/Seagate\ Expansion\ Drive"
+HDPath="/run/media/bruno/Seagate"
 HDYEARMONTH="$HDPath/$YEARMONTH"
-
-White="\e[97m"
-Gray="\e[39m"
 
 mkdir -p "$HDPath/$YEAR"
 mkdir -p "$HDYEARMONTH"
@@ -23,37 +20,36 @@ echo
 echo Press enter key to start the backup
 read
 echo
-echo -e "$White`date +%r` - Copying Dropbox folder (1/7)...$Gray"
+
+echo -e "\e[97m`date +%r` - Copying Dropbox folder (1/7)...\e[39m"
 eval $backup "/run/media/bruno/Multimedia/Dropbox" "$HDYEARMONTH/Multimedia/" || echo ""
 
-echo -e "$White`date +%r` - Copying Linux Home folder (2/7)...$Gray"
+echo -e "\e[97m`date +%r` - Copying Linux Home folder (2/7)...\e[39m"
 
-echo -e "$White            `date +%r` - Bruno$Gray"
+echo -e "\e[97m            `date +%r` - Bruno\e[39m"
 eval $backup "/home/bruno/" "$HDYEARMONTH/LinuxHome/bruno/" || echo ""
 
-echo -e "$White            `date +%r` - Admin$Gray"
+echo -e "\e[97m            `date +%r` - Admin\e[39m"
 eval $backup "/home/admin/" "$HDYEARMONTH/LinuxHome/admin/" || echo ""
 
-echo -e "$White`date +%r` - Copying Localização folder (3/7)...$Gray"
+echo -e "\e[97m            `date +%r` - Lost+Found\e[39m"
+eval $backup "/home/lost+found/" "$HDYEARMONTH/LinuxHome/lost+found/" || echo ""
+
+echo -e "\e[97m`date +%r` - Copying Localização folder (3/7)...\e[39m"
 eval $backup "/run/media/bruno/Multimedia/Localização" "$HDYEARMONTH/Multimedia/" || echo ""
 
-echo -e "$White`date +%r` - Copying My Documents folder (4/7)...$Gray"
-eval $backup "/run/media/bruno/Multimedia/My\ Documents" "$HDYEARMONTH/Multimedia/" || echo ""
+echo -e "\e[97m`date +%r` - Copying My Documents folder (4/7)...\e[39m"
+eval $backup "/run/media/bruno/Multimedia/MyDocuments" "$HDYEARMONTH/Multimedia/" || echo ""
 
-echo -e "$White`date +%r` - Copying Música folder (5/7)...$Gray"
+echo -e "\e[97m`date +%r` - Copying Música folder (5/7)...\e[39m"
 eval $backup "/run/media/bruno/Multimedia/Música" "$HDYEARMONTH/Multimedia/" || echo ""
 
-echo -e "$White`date +%r` - Copying Fotos folder (6/7)...$Gray"
+echo -e "\e[97m`date +%r` - Copying Fotos folder (6/7)...\e[39m"
 eval $backup "/run/media/bruno/Multimedia/Fotos" "$HDPath/$YEAR/" || echo ""
 
-echo -e "$White`date +%r` - Copying Virtual Machines folder (7/7)...$Gray"
+echo -e "\e[97m`date +%r` - Copying Virtual Machines folder (7/7)...\e[39m"
 eval $backup "/run/media/bruno/Multimedia/Virtual\ Machines" "$HDPath/$YEAR/" || echo ""
 
-echo -e "$White`date +%r` - Backup finished. Please, verify your log files.$Gray"
+echo -e "\e[97m`date +%r` - Backup finished. Please, verify your log files.\e[39m"
 
 kdialog --title "Backup Complete" --msgbox "Backup finished successfully"
-
-echo 
-echo Press enter key to close
-read
-
--- Backup/runBackup.sh
+++ Backup/runBackup.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-terminator -m -T BACKUP -x systemd-inhibit sudo bash .Backup_HD_1TB.sh
+terminator -m -T BACKUP -x time systemd-inhibit sudo bash .Backup_HD_1TB.sh; echo; echo "Press enter key to close"; read
+
