428fe8ce78c4bd6307b7d3a0250fe672eb080188
Bruno Fontes <developer@brunofontes.net>
2019-07-15 13:30:28 -0300

    Organizing Backup scripts

---
+++ Services/backup-db.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sshKey=$(<.sshKey)
+serverBackupPath=$(<.serverBackupPath)
+localPath=$(<.localBackupPath)
+
+/bin/rsync -r -e "ssh -i $sshKey" "$serverBackupPath/db/*" "$localPath/db/"
+
+# Delete files older than 15 days
+find "$localPath/db" -type f -name "*.sql.gz" -mtime +15 -delete
---
+++ Services/backup-files.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sshKey=$(<.sshKey)
+serverBackupPath=$(<.serverBackupPath)
+localPath=$(<.localBackupPath)
+
+/bin/rsync -r -e "ssh -i $sshKey" "$serverBackupPath/files/*" "$localPath/files/"
+
+# Delete files older than 30 days
+find "$localPath/files" -type f -name "*.*" -mtime +30 -delete
