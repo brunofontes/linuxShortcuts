c45afbc817b0d8bfc469d95ee328c70f537fb5eb
Bruno Fontes <developer@brunofontes.net>
2021-03-26 21:19:36 -0300

    chore(tmuxPMovase): improve tmux organization/sizes

--- tmuxStartPMOvase.sh
+++ tmuxStartPMOvase.sh
@@ -15,20 +15,24 @@ tmux splitw -h -t pmovase:0.1
 
 terminator -T PMova-se -b --geometry=627x927 -x tmux a -t pmovase &
 
-tmux resize-pane -t pmovase:0.0 -U 30
-tmux resize-pane -t pmovase:0.1 -U 15
+tmux resize-pane -t pmovase:0.0 -U 35
+tmux resize-pane -t pmovase:0.1 -U 10
 tmux resize-pane -t pmovase:0.4 -U 10
 
 tmux send -t pmovase:0.0 $'npm run vagrant; npm run watch\n'
-tmux send -t pmovase:0.2 $'clear; Waiting for vagrant to start...\n'
+tmux send -t pmovase:0.2 $'clear; echo Waiting for vagrant to start...\n'
 
 while [[ -z $(ping projetomovase.test -c 1 | grep "time=") ]] ; do
-    sleep 1
+    sleep 5
 done
+sleep 10
 
+tmux send -t pmovase:0.1 $'npm run cypress:open\n'
 tmux send -t pmovase:0.1 $'npm run test-watch\n'
 tmux send -t pmovase:0.2 $'^u'
-tmux send -t pmovase:0.2 $'phpunit-watcher watch\n'
-tmux send -t pmovase:0.3 $'ssh homestead -t \"cd code;alias clear='clear; figlet Vagrant';clear;bash --login\"\n'
-tmux send -t pmovase:0.4 $'alias clear='clear;figlet PMova-se';clear\n'
+tmux send -t pmovase:0.2 $'npm run test-php-watch\n'
+tmux send -t pmovase:0.3 $'npm run ssh\n'
+tmux send -t pmovase:0.4 $'alias clear=\'clear;figlet PMova-se\';clear\n'
 
+/bin/vscodium &
+/home/bruno/Apps/firefox/firefox-bin &
