ebc7031a3d298900ebb542d0dd3be013f2111415
Bruno Fontes <developer@brunofontes.net>
2021-07-31 16:14:03 -0300

    feat(Browser): Include incognito window option

--- browser.sh
+++ browser.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 activity=`kactivities-cli --current-activity | cut -d" " -f3`
 
-[[ $activity == "OXO" ]] && /bin/brave "$1"
-[[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin "$1"
-[[ $activity == "Main" ]] && /bin/firefox "$1"
-[[ $activity == "Videos" ]] && /bin/firefox "$1"
+if [[ $1 =~ .*\&incognitottt$ ]]; then
+    url="${1/\&incognitottt/}"
+    [[ $activity == "OXO" ]] && /bin/brave --incognito "$url" 2>%1 1>/dev/null &
+    [[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin --private-window "$url" 2>%1 1>/dev/null &
+    [[ $activity == "Main" ]] && /bin/firefox --private-window "$url" 2>%1 1>/dev/null &
+    [[ $activity == "Videos" ]] && /bin/firefox --private-window "$url" 2>%1 1>/dev/null &
+else
+    [[ $activity == "OXO" ]] && /bin/brave "$1" 2>%1 1>/dev/null &
+    [[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin "$1" 2>%1 1>/dev/null &
+    [[ $activity == "Main" ]] && /bin/firefox "$1" 2>%1 1>/dev/null &
+    [[ $activity == "Videos" ]] && /bin/firefox "$1" 2>%1 1>/dev/null &
+fi
