0c16db897ffd22fe8ad8fc95e2e880bcc8782f48
Bruno Fontes <developer@brunofontes.net>
2021-08-04 17:15:18 -0300

    fix(browser): Fix typo redirecting errors do /dev/null

--- browser.sh
+++ browser.sh
@@ -3,13 +3,13 @@ activity=`kactivities-cli --current-activity | cut -d" " -f3`
 
 if [[ $1 =~ .*\&incognitottt$ ]]; then
     url="${1/\&incognitottt/}"
-    [[ $activity == "OXO" ]] && /bin/brave --incognito "$url" 2>%1 1>/dev/null &
-    [[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin --private-window "$url" 2>%1 1>/dev/null &
-    [[ $activity == "Main" ]] && /bin/firefox --private-window "$url" 2>%1 1>/dev/null &
-    [[ $activity == "Videos" ]] && /bin/firefox --private-window "$url" 2>%1 1>/dev/null &
+    [[ $activity == "OXO" ]] && /bin/brave --incognito "$url" >/dev/null 2>&1 &
+    [[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin --private-window "$url" >/dev/null 2>&1 &
+    [[ $activity == "Main" ]] && /bin/firefox --private-window "$url" >/dev/null 2>&1 &
+    [[ $activity == "Videos" ]] && /bin/firefox --private-window "$url" >/dev/null 2>&1 &
 else
-    [[ $activity == "OXO" ]] && /bin/brave "$1" 2>%1 1>/dev/null &
-    [[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin "$1" 2>%1 1>/dev/null &
-    [[ $activity == "Main" ]] && /bin/firefox "$1" 2>%1 1>/dev/null &
-    [[ $activity == "Videos" ]] && /bin/firefox "$1" 2>%1 1>/dev/null &
+    [[ $activity == "OXO" ]] && /bin/brave "$1" >/dev/null 2>&1 &
+    [[ $activity == "Development" ]] && /home/bruno/Apps/firefox/firefox-bin "$1" >/dev/null 2>&1 &
+    [[ $activity == "Main" ]] && /bin/firefox "$1" >/dev/null 2>&1 &
+    [[ $activity == "Videos" ]] && /bin/firefox "$1" >/dev/null 2>&1 &
 fi
